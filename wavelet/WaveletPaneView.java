2369d5f4f1632cebb57b4f33f40346a0861a0429
西澤万葉 <tmkgt@nishizawamahas-MacBook-Pro.local>
2025-05-30 16:43:39 +0900

    feat:WabletPainViewを作成

--- wavelet/WaveletPaneView.java
+++ wavelet/WaveletPaneView.java
@@ -1,19 +1,46 @@
 package wavelet;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import wavelet.PaneView;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+
 
 public class WaveletPaneView extends PaneView {
+	
 
 	public WaveletPaneView(WaveletPaneModel aModel) {
+		super(aModel);
 
 	}
 
 	public WaveletPaneView(WaveletPaneModel aModel, WaveletPaneController aController) {
-
+		super(aModel, aController);
 	}
 
-	public void paintComponent(Graphics aGraohics) {
+	
+	protected void paintComponent(Graphics aGraphics) {
+    	
+		
+    	Font font = new Font("MonoSpaced", 0, 12);
+    	aGraphics.setFont(font);
+    	WaveletPaneModel waveletPaneModel = (WaveletPaneModel)getModel();
+   		int i = font.getSize();
+    	String str = waveletPaneModel.label();
 
-	}
+    	aGraphics.setColor(Color.white);
+    	aGraphics.drawString(str, 1, i + 1);
+    	aGraphics.drawString(str, 2, i + 1);
+    	aGraphics.drawString(str, 3, i + 1);
+    	aGraphics.drawString(str, 1, i + 2);
+    	aGraphics.drawString(str, 3, i + 2);
+    	aGraphics.drawString(str, 1, i + 3);
+    	aGraphics.drawString(str, 2, i + 3);
+    	aGraphics.drawString(str, 3, i + 3);
+    	aGraphics.setColor(Color.black);
+    	aGraphics.drawString(str, 2, i + 2);
+}
 
 }
