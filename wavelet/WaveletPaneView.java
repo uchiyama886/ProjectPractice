5eb451b62fb474d05580418b12ac0dca1598fa2f
西澤万葉 <tmkgt@nishizawamahas-MacBook-Pro.local>
2025-06-06 14:03:18 +0900

    feat:修正

--- wavelet/WaveletPaneView.java
+++ wavelet/WaveletPaneView.java
@@ -4,15 +4,11 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import wavelet.PaneView;
-import java.awt.geom.Point2D;
-import java.awt.image.BufferedImage;
-
 
 public class WaveletPaneView extends PaneView {
-	
 
 	public WaveletPaneView(WaveletPaneModel aModel) {
-		super(aModel);
+		super(aModel, new WaveletPaneController());
 
 	}
 
@@ -20,27 +16,25 @@ public class WaveletPaneView extends PaneView {
 		super(aModel, aController);
 	}
 
-	
 	protected void paintComponent(Graphics aGraphics) {
-    	
-		
-    	Font font = new Font("MonoSpaced", 0, 12);
-    	aGraphics.setFont(font);
-    	WaveletPaneModel waveletPaneModel = (WaveletPaneModel)getModel();
-   		int i = font.getSize();
-    	String str = waveletPaneModel.label();
 
-    	aGraphics.setColor(Color.white);
-    	aGraphics.drawString(str, 1, i + 1);
-    	aGraphics.drawString(str, 2, i + 1);
-    	aGraphics.drawString(str, 3, i + 1);
-    	aGraphics.drawString(str, 1, i + 2);
-    	aGraphics.drawString(str, 3, i + 2);
-    	aGraphics.drawString(str, 1, i + 3);
-    	aGraphics.drawString(str, 2, i + 3);
-    	aGraphics.drawString(str, 3, i + 3);
-    	aGraphics.setColor(Color.black);
-    	aGraphics.drawString(str, 2, i + 2);
-}
+		super.paintComponent(aGraphics);
+		Font font = new Font("MonoSpaced", 0, 12);
+		aGraphics.setFont(font);
+		WaveletPaneModel waveletPaneModel = (WaveletPaneModel) getModel();
+		int i = font.getSize();
+		String str = waveletPaneModel.label();
 
+		aGraphics.setColor(Color.white);
+		aGraphics.drawString(str, 1, i + 1);
+		aGraphics.drawString(str, 2, i + 1);
+		aGraphics.drawString(str, 3, i + 1);
+		aGraphics.drawString(str, 1, i + 2);
+		aGraphics.drawString(str, 3, i + 2);
+		aGraphics.drawString(str, 1, i + 3);
+		aGraphics.drawString(str, 2, i + 3);
+		aGraphics.drawString(str, 3, i + 3);
+		aGraphics.setColor(Color.black);
+		aGraphics.drawString(str, 2, i + 2);
+	}
 }
