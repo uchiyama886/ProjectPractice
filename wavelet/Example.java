93150f96fbd58b355dff7d5cf3e5a5ab7738e7f4
Yuto2004 <anyueri1206@gmail.com>
2025-06-06 17:25:12 +0900

    refactor

--- wavelet/Example.java
+++ wavelet/Example.java
@@ -1,9 +1,17 @@
 package wavelet;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Example extends Object {
 
 	public static void main(String[] arguments) {
 
 	}
 
+	
 }
